44d287f4f9512cd0273547818cbd30ebe8e15562
Enoc Castro <enocco70@estudiantec.cr>
2021-11-02 17:41:11 -0600

    form get values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario !: NgForm;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(){
+    console.log(this.miFormulario.value);
   }
 
 }
