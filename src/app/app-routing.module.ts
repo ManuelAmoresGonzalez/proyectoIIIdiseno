42094562c5bd5650e4557d4061f21f73019402ff
Manuel Amores González <amoresgonzalezmanuel@gmail.com>
2021-11-02 20:55:55 -0600

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { VistaServicioComponent } from './vista-servicio/vista-servicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'vistaservicio', component: VistaServicioComponent},
+  {path: '**', pathMatch: 'full', redirectTo:'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,18 @@ export class LoginComponent implements OnInit {
   @ViewChild('miFormulario') miFormulario !: NgForm;
 
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
   guardar(){
-    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.value.user);
+    console.log(this.miFormulario.value.password);
+    if(this.miFormulario.value.user == "Hola" && this.miFormulario.value.password == "hola"){
+      this.router.navigate(['vistaservicio']);
+    }else{
+      console.log("No se puede entrar")
+    }    
   }
-
 }
